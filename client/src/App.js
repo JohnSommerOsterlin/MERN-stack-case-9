1b3817fc7b7757562df8cd704e19b5dcbce7edb8
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-12-08 12:49:36 +0100

    added components folder and navbar to app.js and added pages folder and home.js file to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
+// Dependencies
 import { BrowserRouter, Routes, Route} from "react-router-dom"
+
+// Pages
+import Home from "./pages/Home"
+
+// Components
+import Navbar from "./components/Navbar"
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="pages">
-          
+      <Navbar />
+        <div className="pages px-10 py-6">
+          <Routes>
+            <Route 
+            path="/"
+            element={<Home />}
+            />
+          </Routes>
         </div>
       </BrowserRouter>
     </div>
