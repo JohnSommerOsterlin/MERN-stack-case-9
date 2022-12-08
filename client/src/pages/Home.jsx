ef96bcec44ebdd8486a4d1dd35f146a0cb8a459a
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-12-08 16:10:10 +0100

    created pages and components folders. fetching data from localholst:5000 and rendering it out in home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+
+
 const Home = () => {
+    const [posts, setPosts] = useState(null)
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await fetch("http://localhost:5000/api/posts")
+            const json = await response.json()
+
+            if (response.ok) {
+                setPosts(json)
+            }
+        }
+
+        fetchPost()
+    }, [])
+
+
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            <div className="posts ">
+                {posts && posts.map((post) => (
+                    <p key={post._id}>{post.username}</p>
+                ))}
+            </div>
         </div>
-    )
-    ;
+    );
 }
 
 export default Home;
