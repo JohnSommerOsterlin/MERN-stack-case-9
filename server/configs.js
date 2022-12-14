c0e68c942309eb34550010a82933f91b7ada4c43
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-12-14 12:45:15 +0100

    added and handeling SECRET to .env for unique webtoken

--- server/configs.js
+++ server/configs.js
@@ -5,7 +5,8 @@ const config = dotenv.config().parsed;
 
 const PORT = Number(config.PORT);
 const MONGO_URI = config.MONGO_URI;
+const SECRET = config.SECRET
 
 
 
-export {config, PORT, MONGO_URI};
+export {config, PORT, MONGO_URI, SECRET};
