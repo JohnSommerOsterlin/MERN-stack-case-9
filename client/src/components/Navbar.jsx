cd14041a078560e683bf29aa80cd3dfba16c0f1d
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-12-16 10:14:59 +0100

    react checking if there is a user in the local storage and updating the state to display user in nav bar when refreshing page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "../images/dog.svg"
+
+// Hooks
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
     const { logout } = useLogout()
+    const { user } =useAuthContext()
 
     const handleClick = () => {
         logout()
@@ -11,20 +15,23 @@ const Navbar = () => {
     return ( 
         <header className="bg-white w-screen px-10 shadow-md">
             <div className="flex align-middle justify-between py-3">
-                <Link className="text-gray-800" to="/">
-                    <img src={logo} alt="logotype" className="w-14 font-bold text-2xl"/>
+                <Link className="flex" to="/">
+                    <img src={logo} alt="logotype" className=" w-16"/>
                 </Link>
-                <div className="flex space-x-4">
-                    <div className="logout-container">
-                        <button className="mt-1.5 border-2 text-sm px-6 h-8      rounded-lg" onClick={handleClick}>Log out</button>
-                    </div>
+                <div className="flex">
+                    {user && (<div className="logout-container text-end">
+                    <span className="">Welcome {user.username}!</span>
+                    <button className="ml-10 mt-1.5 border-2 w-28 h-8 rounded-lg" 
+                    onClick={handleClick}>Sign out</button>
+                </div>)}
+            
 
-                    <div className="login-container flex align-middle">
+                    {!user && (<div className="login-container flex align-middle">
                         <Link 
                             className="mt-1.5 border-2 text-sm px-6 py-1 h-8 rounded-lg"  to="/login">
                             Log in
                         </Link>
-                    </div>
+                    </div>)}
                 </div>
             </div>
         </header>
