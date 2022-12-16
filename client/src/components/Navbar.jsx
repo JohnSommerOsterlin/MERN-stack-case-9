db9d8cc1b0e67c12688a5cc61e05a8c357eb31fe
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2022-12-16 09:19:18 +0100

    added functionality to log out a user and remove the webtoken

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,31 @@
 import { Link } from "react-router-dom";
 import logo from "../images/dog.svg"
+import { useLogout } from "../hooks/useLogout";
 
 const Navbar = () => {
+    const { logout } = useLogout()
+
+    const handleClick = () => {
+        logout()
+    }
     return ( 
         <header className="bg-white w-screen px-10 shadow-md">
             <div className="flex align-middle justify-between py-3">
                 <Link className="text-gray-800" to="/">
                     <img src={logo} alt="logotype" className="w-14 font-bold text-2xl"/>
                 </Link>
-                <Link className="mt-1.5 border-2 pb-0 text-lg px-6 h-8 rounded-lg" to="/login">
-                    Log in
-                </Link>
+                <div className="flex space-x-4">
+                    <div className="logout-container">
+                        <button className="mt-1.5 border-2 text-sm px-6 h-8      rounded-lg" onClick={handleClick}>Log out</button>
+                    </div>
+
+                    <div className="login-container flex align-middle">
+                        <Link 
+                            className="mt-1.5 border-2 text-sm px-6 py-1 h-8 rounded-lg"  to="/login">
+                            Log in
+                        </Link>
+                    </div>
+                </div>
             </div>
         </header>
     );
